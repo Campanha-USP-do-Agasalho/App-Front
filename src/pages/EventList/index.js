bd7dfd32d67d6fe4c3607c97ac4e72c7609bed51
JVGC <joaogarcia@usp.br>
2021-01-27 19:53:17 -0300

    commit pre update do expo

--- src/pages/EventList/index.js
+++ src/pages/EventList/index.js
@@ -9,6 +9,8 @@ import {
   StatusBar,
 } from 'react-native';
 
+import { Picker } from '@react-native-picker/picker';
+
 import { FontAwesome } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import ShimmerPlaceHolder from 'react-native-shimmer-placeholder';
@@ -20,9 +22,44 @@ import globalStyles, { colors } from '../../globalStyles';
 export default function EventList() {
   const navigation = useNavigation();
 
+  const [allEvents, setAllEvents] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState([]);
   const [name, setName] = useState('');
+  const [eventGroup, setEventGroup] = useState('all');
   const [team, setTeam] = useState('all');
   const [loaded, setLoaded] = useState(true);
+
+  function filterEvents(events, searchName, searchTeam, searchEventGroup) {
+    const newData = events.filter((event) => {
+      const textData = searchName.toUpperCase();
+      if (team === 'all') {
+        if (eventGroup === 'all') {
+          return (event.name.indexOf(textData) > -1);
+        }
+        return (
+          event.name.indexOf(textData) > -1
+                && event.group.name.toUpperCase() === searchEventGroup.toUpperCase()
+        );
+      }
+
+      if (eventGroup === 'all') {
+        return (event.name.indexOf(textData) > -1
+        && event.team.name.toUpperCase() === searchTeam.toUpperCase());
+      }
+
+      return (event.name.indexOf(textData) > -1
+        && event.team.name.toUpperCase() === searchTeam.toUpperCase()
+        && event.group.name.toUpperCase() === searchEventGroup.toUpperCase());
+    });
+
+    return newData;
+  }
+
+  function filterEventsbyName(newName) {
+    setName(newName);
+    const newData = filterEvents(allEvents, newName, team, eventGroup);
+    setFilteredEvents(newData);
+  }
 
   return (
     <View style={globalStyles.container}>
@@ -47,13 +84,64 @@ export default function EventList() {
 
             <TextInput
               autoCapitalize="words" // sem a primeira letra maiuscula
-              placeholder="Nome do evento"
+              placeholder="Nome do Evento"
               placeholderTextColor={colors.grey3}
               style={styles.inputText}
-              onChangeText={(name) => filterMembersbyName(name)}
+              value={name}
+              onChangeText={(name) => filterEventsbyName(name)}
             />
             <FontAwesome name="search" color={colors.grey2} size={28} />
 
+          </View>
+        </ShimmerPlaceHolder>
+        {/* VIEW COM OS FILTROS DE CARRO E NÚCLEO */}
+        <ShimmerPlaceHolder
+          style={styles.filterPlaceholder}
+          autoRun
+          visible={loaded}
+        >
+          <View style={styles.filter}>
+            <View style={styles.PickerView}>
+              <Picker
+                selectedValue={team}
+                onValueChange={(itemValue) => {
+                  filterEventsbyTeam(itemValue);
+                }}
+                style={styles.Picker}
+              >
+                <Picker.Item
+                  color={colors.grey3}
+                  value="all"
+                  label="Grupo"
+                />
+                <Picker.Item label="Entidades" value="Entidades" />
+                <Picker.Item label="Divulgação" value="Divulgação" />
+                <Picker.Item label="Infra" value="Infraestrutura" />
+                <Picker.Item label="RE" value="Relações Externas" />
+                <Picker.Item label="Geral" value="Geral" />
+              </Picker>
+            </View>
+
+            <View style={styles.PickerView}>
+              <Picker
+                selectedValue={team}
+                onValueChange={(itemValue) => {
+                  filterEventsbyTeam(itemValue);
+                }}
+                style={styles.Picker}
+              >
+                <Picker.Item
+                  color={colors.grey3}
+                  value="all"
+                  label="Núcleo"
+                />
+                <Picker.Item label="Entidades" value="Entidades" />
+                <Picker.Item label="Divulgação" value="Divulgação" />
+                <Picker.Item label="Infra" value="Infraestrutura" />
+                <Picker.Item label="RE" value="Relações Externas" />
+                <Picker.Item label="Geral" value="Geral" />
+              </Picker>
+            </View>
           </View>
         </ShimmerPlaceHolder>
       </View>
--- src/pages/EventList/styles.js
+++ src/pages/EventList/styles.js
@@ -33,9 +33,43 @@ export default StyleSheet.create({
   inputText: {
     alignSelf: 'stretch',
     paddingHorizontal: 16,
+    fontSize: 16,
   },
   searchIcon: {
     alignSelf: 'auto',
+  },
+  filter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginTop: 13,
+  },
+  filterPlaceholder: {
+    height: 0.15 * height,
+    width: 0.9 * width,
+    marginTop: 13,
+    alignSelf: 'center',
+  },
+
+  PickerView: {
+    marginLeft: 5,
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: colors.black,
+    overflow: 'hidden',
+  },
+  Picker: {
+    backgroundColor: colors.white,
+    height: 0.08 * height,
+    width: 0.48 * width,
+    paddingHorizontal: 0.027 * width,
+  },
+  line: {
+    backgroundColor: colors.black,
+    height: 1,
+    width: 0.8 * width,
+    alignSelf: 'center',
+    margin: 5,
   },
 
 });
